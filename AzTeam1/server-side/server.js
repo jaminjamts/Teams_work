1cb318e7204b6126e1006524d3ace4b79c494443
Жаминжамц Батсундуй <24LP6854@Admins-iMac.local>
2024-09-26 14:07:14 +0800

    class end

--- AzTeam1/server-side/server.js
+++ AzTeam1/server-side/server.js
@@ -1,17 +1,54 @@
-import express from "express";
+import express, { response } from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 const port = 9999;
 const app = express();
 
 app.use(cors());
-app.use();
-app.get("/", (request, response) => {
-  response.send("Hellosdadsadsds");
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.post("/", (request, response) => {
+  const { username, password } = request.body;
+  const registeredUser = users.filter(
+    (user) => user.username === username && user.password === password
+  );
+  if (registeredUser.length > 0) {
+    response.json({
+      success: true,
+    });
+  } else {
+    response.json({
+      success: false,
+    });
+  }
 });
 
-app.post("/", (req, res) => {
-  res.send("post");
+app.post("/register", (request, response) => {
+  users.push(request.body);
+  response.send("user amjilttai newterlee");
+});
+
+app.post("./data/user.json", "utf-8", (readError, data) => {
+  let savedData = JSON.parse(data);
+  if (readError) {
+    response.json({
+      status: "read file error",
+    });
+  }
+  const newUser = {
+    id: Date.now().toString(),
+    username: body.username,
+    age: body.age,
+  };
+  savedData.push(newUser);
+
+  fs.writeFile("./data/user.json", JSON.stringify(savedData), (writeError) => {
+    if (writeError) {
+      response.json({ status: "error" });
+    }
+  });
 });
 
 app.listen(port, () => {
